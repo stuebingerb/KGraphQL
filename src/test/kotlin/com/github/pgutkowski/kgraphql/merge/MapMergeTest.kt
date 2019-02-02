9e3d582b0ed36699c062fd46a51f830965c8bc7d
Michael D. Norman <michael.norman@denormans.com>
2019-02-02 16:28:29 -0600

    [issue-50] added merge tests

---
+++ src/test/kotlin/com/github/pgutkowski/kgraphql/merge/MapMergeTest.kt
@@ -0,0 +1,69 @@
+package com.github.pgutkowski.kgraphql.merge
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.JsonNodeFactory
+import com.github.pgutkowski.kgraphql.expect
+import com.github.pgutkowski.kgraphql.schema.execution.merge
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class MapMergeTest {
+    private val jsonNodeFactory = JsonNodeFactory.instance
+
+    @Test
+    fun `merge should add property`() {
+        val existing = createMap("param1" to jsonNodeFactory.textNode("value1"))
+        val update: JsonNode? = jsonNodeFactory.textNode("value2")
+
+        existing.merge("param2", update)
+
+        assertThat(existing.get("param2"), equalTo(update))
+    }
+
+    @Test
+    fun `merge should add nested property`() {
+        val existing = createMap("param1" to jsonNodeFactory.textNode("value1"))
+        val update: JsonNode? = jsonNodeFactory.objectNode().put("param2", "value2")
+
+        existing.merge("sub", update)
+
+        assertThat(existing.get("sub"), equalTo(update))
+    }
+
+    @Test
+    fun `merge should not change simple node`() {
+        val existingValue: JsonNode? = jsonNodeFactory.textNode("value1")
+        val existing = createMap("param" to existingValue)
+        val update = jsonNodeFactory.textNode("value2")
+
+        expect<IllegalStateException>("different simple nodes") { existing.merge("param", update) }
+
+        assertThat(existing.get("param"), equalTo(existingValue))
+    }
+
+    @Test
+    fun `merge should not merge simple node with object node`() {
+        val existingValue: JsonNode? = jsonNodeFactory.textNode("value1")
+        val existing = createMap("param" to existingValue)
+        val update = jsonNodeFactory.objectNode()
+
+        expect<IllegalStateException>("merge object with simple node") { existing.merge("param", update) }
+
+        val expected: JsonNode? = jsonNodeFactory.textNode("value1")
+        assertThat(existing.get("param"), equalTo(expected))
+    }
+
+    @Test
+    fun `merge should not merge object node with simple node`() {
+        val existingObj: JsonNode? = jsonNodeFactory.objectNode().put("other", "value1")
+        val existing = createMap("param" to existingObj)
+        val update = jsonNodeFactory.textNode("value2")
+
+        expect<IllegalStateException>("merge simple node with object node") { existing.merge("param", update) }
+
+        assertThat(existing.get("param"), equalTo(existingObj))
+    }
+
+    private fun createMap(vararg pairs: Pair<String, JsonNode?>) = mutableMapOf(*pairs)
+}
