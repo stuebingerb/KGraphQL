8f7b33f36291ce8849672f42e52dc3696cdfd07f
Jógvan Olsen <jol@apurebase.com>
2019-04-29 15:52:15 +0100

    Support for suspendable property resolvers

--- src/test/kotlin/com/apurebase/kgraphql/integration/ParallelExecutionTest.kt
+++ src/test/kotlin/com/apurebase/kgraphql/integration/ParallelExecutionTest.kt
@@ -1,36 +1,68 @@
 package com.apurebase.kgraphql.integration
 
 import com.apurebase.kgraphql.KGraphQL
-import com.apurebase.kgraphql.assertNoErrors
 import com.apurebase.kgraphql.extract
 import com.apurebase.kgraphql.deserialize
 import kotlinx.coroutines.delay
 import org.hamcrest.CoreMatchers
 import org.hamcrest.MatcherAssert
 import org.junit.Test
+import kotlin.random.Random
 
 class ParallelExecutionTest {
 
-    val syncResolversSchema = KGraphQL.schema {
+    data class AType(val id: Int)
+
+    private val syncResolversSchema = KGraphQL.schema {
         repeat(1000) {
-            query("automated-${it}") {
+            query("automated-$it") {
                 resolver { ->
                     Thread.sleep(3)
-                    "${it}"
+                    "$it"
                 }
             }
         }
     }
 
-    val suspendResolverSchema = KGraphQL.schema {
+    private val suspendResolverSchema = KGraphQL.schema {
         repeat(1000) {
-            query("automated-${it}") {
+            query("automated-$it") {
                 suspendResolver { ->
                     delay(3)
-                    "${it}"
+                    "$it"
                 }
             }
         }
+    }
+
+    private val suspendPropertySchema = KGraphQL.schema {
+        query("getAll") {
+            resolver { -> (0..999).map { AType(it) } }
+        }
+        type<AType> {
+            property<List<AType>>("children") {
+                suspendResolver { parent ->
+                    (0..50).map {
+                        delay(Random.nextLong(1, 100))
+                        AType((parent.id * 10) + it)
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `Suspendable property resolvers`() {
+        val query = "{getAll{id,children{id}}}"
+        val map = deserialize(suspendPropertySchema.execute(query))
+
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[0]/id"), CoreMatchers.equalTo(0))
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[500]/id"), CoreMatchers.equalTo(500))
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[766]/id"), CoreMatchers.equalTo(766))
+
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[5]/children[5]/id"), CoreMatchers.equalTo(55))
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[75]/children[9]/id"), CoreMatchers.equalTo(759))
+        MatcherAssert.assertThat(map.extract<Int>("data/getAll[888]/children[50]/id"), CoreMatchers.equalTo(8930))
     }
 
     val query = "{ " + (0..999).map { "automated-${it}" }.joinToString(", ") + " }"
