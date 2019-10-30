7c55c80a1cde38d82658bfc373a51898754a6293
Allali84 <allali.elkhalili@gmail.com>
2019-10-30 20:15:10 +0100

    Fix an import bug

--- src/test/kotlin/com/apurebase/kgraphql/specification/language/OperationsSpecificationTest.kt
+++ src/test/kotlin/com/apurebase/kgraphql/specification/language/OperationsSpecificationTest.kt
@@ -1,23 +1,41 @@
 package com.apurebase.kgraphql.specification.language
 
-import com.apurebase.kgraphql.Actor
-import com.apurebase.kgraphql.Specification
-import com.apurebase.kgraphql.defaultSchema
-import com.apurebase.kgraphql.executeEqualQueries
-import junit.framework.Assert.fail
-import org.junit.Ignore
+import com.apurebase.kgraphql.*
+import com.apurebase.kgraphql.schema.dsl.subscribe
+import com.apurebase.kgraphql.schema.dsl.unsubscribe
+import org.junit.Assert
 import org.junit.Test
+
+data class Actor(var name : String? = "", var age: Int? = 0)
 
 @Specification("2.3 Operations")
 class OperationsSpecificationTest {
 
+    var subscriptionResult = ""
+
     val schema = defaultSchema {
+
         query("fizz") {
-            resolver{ -> "buzz"}
+            resolver{ -> "buzz"}.withArgs {  }
         }
 
-        mutation("createActor") {
+        val publisher = mutation("createActor") {
             resolver { name : String -> Actor(name, 11) }
+        }
+
+        subscription("subscriptionActor") {
+            resolver { subscription: String ->
+                subscribe(subscription, publisher, Actor()) {
+                    subscriptionResult = it
+                    println(it)
+                }
+            }
+        }
+
+        subscription("unsubscriptionActor") {
+            resolver { subscription: String ->
+                unsubscribe(subscription, publisher, Actor())
+            }
         }
     }
 
@@ -42,8 +60,26 @@ class OperationsSpecificationTest {
     }
 
     @Test
-    @Ignore("Feature not supported yet")
     fun `handle subscription`(){
-        fail("Feature not supported yet")
+        schema.execute("subscription {subscriptionActor(subscription : \"mySubscription\"){name}}")
+
+        subscriptionResult = ""
+        schema.execute("{createActor(name : \"Kurt Russel\"){name}}")
+        Assert.assertEquals(subscriptionResult, "{\"data\":{\"name\":\"Kurt Russel\"}}")
+
+        subscriptionResult = ""
+        schema.execute("{createActor(name : \"Kurt Russel1\"){name}}")
+        Assert.assertEquals(subscriptionResult, "{\"data\":{\"name\":\"Kurt Russel1\"}}")
+
+        subscriptionResult = ""
+        schema.execute("{createActor(name : \"Kurt Russel2\"){name}}")
+        Assert.assertEquals(subscriptionResult, "{\"data\":{\"name\":\"Kurt Russel2\"}}")
+
+        schema.execute("subscription {unsubscriptionActor(subscription : \"mySubscription\"){name}}")
+
+        subscriptionResult = ""
+        schema.execute("{createActor(name : \"Kurt Russel\"){name}}")
+        Assert.assertEquals(subscriptionResult, "")
+
     }
 }
