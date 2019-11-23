beca33bc6f917243fa344757e6c694d51aba1550
Niclas Roßberger <niclas@nidomiro.de>
2019-11-23 09:07:41 +0100

    moves internal to constructor of SchemaBuilder

--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/SchemaBuilder.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/SchemaBuilder.kt
@@ -20,7 +20,7 @@ import kotlin.reflect.KClass
 /**
  * SchemaBuilder exposes rich DSL to setup GraphQL schema
  */
-internal class SchemaBuilder {
+class SchemaBuilder internal constructor() {
 
     private val model = MutableSchemaDefinition()
 
