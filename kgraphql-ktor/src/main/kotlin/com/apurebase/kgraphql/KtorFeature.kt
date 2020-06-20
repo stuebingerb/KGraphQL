a81e3e84f55bb4734ea314c462e5b1faf7a2ba84
Jógvan Olsen <jo@jebster.net>
2020-06-20 18:08:13 +0100

    Install Ktor feature and get the rest out of the box

--- kgraphql-ktor/src/main/kotlin/com/apurebase/kgraphql/KtorFeature.kt
+++ kgraphql-ktor/src/main/kotlin/com/apurebase/kgraphql/KtorFeature.kt
@@ -1,54 +1,83 @@
 package com.apurebase.kgraphql
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
-import io.ktor.application.ApplicationCall
-import io.ktor.application.call
-import io.ktor.application.install
-import io.ktor.features.ContentNegotiation
+import com.apurebase.kgraphql.schema.dsl.SchemaConfigurationDSL
+import io.ktor.application.*
 import io.ktor.http.ContentType
-import io.ktor.request.receive
+import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.response.respondBytes
-import io.ktor.response.respondFile
-import io.ktor.routing.Route
-import io.ktor.routing.get
-import io.ktor.routing.post
-import io.ktor.serialization.json
-import io.ktor.util.KtorExperimentalAPI
+import io.ktor.routing.*
+import io.ktor.util.AttributeKey
 import kotlinx.serialization.UnstableDefault
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
-import java.io.File
+import kotlinx.serialization.json.Json.Default.parse
 
-@KtorExperimentalAPI
-@UnstableDefault
-fun Route.graphql(ctxBuilder: ContextBuilder.(ApplicationCall) -> Unit = {}, block: SchemaBuilder.() -> Unit) {
-    val schema = KGraphQL.schema(block)
-    val conf = schema.configuration[KtorGraphQLConfiguration::class] ?: KtorConfigurationDSL().build()
+class GraphQL {
 
 
-    install(ContentNegotiation) {
-        val jsonConfig = Json(JsonConfiguration.Stable.copy(
-                isLenient = true,
-                ignoreUnknownKeys = true,
-                serializeSpecialFloatingPointValues = true,
-                useArrayPolymorphism = true
-        ))
-        register(ContentType.Application.Json, GraphqlSerializationConverter(jsonConfig))
-        json(json = jsonConfig)
+    class Configuration: SchemaConfigurationDSL() {
+        fun schema(block: SchemaBuilder.() -> Unit) {
+            schemaBlock = block
+        }
+
+        /**
+         * This adds support for opening the graphql route within the browser
+         */
+        var playground: Boolean = false
+
+        var endpoint: String = "/graphql"
+
+        fun context(block: ContextBuilder.(ApplicationCall) -> Unit) {
+            contextSetup = block
+        }
+
+        fun wrap(block: Route.(next: Route.() -> Unit) -> Unit) {
+            wrapWith = block
+        }
+
+        internal var contextSetup: (ContextBuilder.(ApplicationCall) -> Unit)? = null
+        internal var wrapWith: (Route.(next: Route.() -> Unit) -> Unit)? = null
+        internal lateinit var schemaBlock: SchemaBuilder.() -> Unit
+
     }
 
-    post(conf.endpoint) {
-        val request = call.receive<GraphqlRequest>()
-        val ctx = context { ctxBuilder(this, call) }
-        val result = schema.execute(request.query, request.variables.toString(), ctx)
-        call.respond(result)
-    }
 
-    if (conf.playground) get(conf.endpoint) {
-        @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-        call.respondBytes(KtorGraphQLConfiguration::class.java.classLoader.getResource("playground.html").readBytes(),
-                contentType = ContentType.Text.Html)
-    }
+    companion object Feature: ApplicationFeature<Application, Configuration, GraphQL> {
+        override val key = AttributeKey<GraphQL>("KGraphQL")
 
+        @UnstableDefault
+        override fun install(pipeline: Application, configure: Configuration.() -> Unit): GraphQL {
+            val config = Configuration().apply(configure)
+            val schema = KGraphQL.schema {
+                configure(config)
+                config.schemaBlock(this)
+            }
+
+            val routing: Routing.() -> Unit = {
+                val routing: Route.() -> Unit = {
+                    route(config.endpoint) {
+                        post {
+                            val request = parse(GraphqlRequest.serializer(), call.receiveText())
+                            val ctx = context {
+                                config.contextSetup?.invoke(this, call)
+                            }
+                            val result = schema.execute(request.query, request.variables.toString(), ctx)
+                            call.respond(result)
+                        }
+                        if (config.playground) get {
+                            @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+                            val playgroundHtml = KtorGraphQLConfiguration::class.java.classLoader.getResource("playground.html").readBytes()
+                            call.respondBytes(playgroundHtml, contentType = ContentType.Text.Html)
+                        }
+                    }
+                }
+
+                config.wrapWith?.invoke(this, routing) ?: routing(this)
+            }
+
+            pipeline.featureOrNull(Routing)?.apply(routing) ?: pipeline.install(Routing, routing)
+
+            return GraphQL()
+        }
+    }
 }
