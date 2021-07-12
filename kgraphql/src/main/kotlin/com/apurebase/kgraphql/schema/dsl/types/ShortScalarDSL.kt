cb0ca7cacd0d16e5b56237c6d6c8824c06af655f
Bertrand Guiheneuf <guiheneuf@gmail.com>
2021-07-13 01:43:25 +0200

    fix(build): missing files

---
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/types/ShortScalarDSL.kt
@@ -0,0 +1,26 @@
+package com.apurebase.kgraphql.schema.dsl.types
+
+
+import com.apurebase.kgraphql.schema.SchemaException
+import com.apurebase.kgraphql.schema.model.ast.ValueNode
+import com.apurebase.kgraphql.schema.scalar.ShortScalarCoercion
+import com.apurebase.kgraphql.schema.scalar.ScalarCoercion
+import kotlin.reflect.KClass
+
+
+class ShortScalarDSL<T : Any>(kClass: KClass<T>) : ScalarDSL<T, Short>(kClass) {
+
+    override fun createCoercionFromFunctions(): ScalarCoercion<T, Short> {
+        return object : ShortScalarCoercion<T> {
+
+            val serializeImpl = serialize ?: throw SchemaException(PLEASE_SPECIFY_COERCION)
+
+            val deserializeImpl = deserialize ?: throw SchemaException(PLEASE_SPECIFY_COERCION)
+
+            override fun serialize(instance: T): Short = serializeImpl(instance)
+
+            override fun deserialize(raw: Short, valueNode: ValueNode?): T = deserializeImpl(raw)
+        }
+    }
+
+}
