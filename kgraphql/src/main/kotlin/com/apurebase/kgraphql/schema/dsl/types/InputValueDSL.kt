68d6c235e5e92b5a4106ed6f09e6a6a3826af46a
Erik <Erik@ErikSchouten.com>
2021-07-29 17:06:17 +0200

    Automatic setting ktype from InputValuesDSL

--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/types/InputValueDSL.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/types/InputValueDSL.kt
@@ -3,9 +3,10 @@ package com.apurebase.kgraphql.schema.dsl.types
 import com.apurebase.kgraphql.schema.dsl.DepreciableItemDSL
 import com.apurebase.kgraphql.schema.model.InputValueDef
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
 
 
-class InputValueDSL<T : Any>(val kClass: KClass<T>) : DepreciableItemDSL() {
+class InputValueDSL<T : Any>(val kClass: KClass<T>, val kType: KType? = null) : DepreciableItemDSL() {
 
     lateinit var name : String
 
@@ -17,6 +18,7 @@ class InputValueDSL<T : Any>(val kClass: KClass<T>) : DepreciableItemDSL() {
             defaultValue = defaultValue,
             isDeprecated = isDeprecated,
             description = description,
-            deprecationReason = deprecationReason
+            deprecationReason = deprecationReason,
+            kType = kType
     )
 }
--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/types/InputValuesDSL.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/dsl/types/InputValuesDSL.kt
@@ -1,19 +1,21 @@
 package com.apurebase.kgraphql.schema.dsl.types
 
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
+import kotlin.reflect.typeOf
 
 
 class InputValuesDSL {
 
     val inputValues = mutableListOf<InputValueDSL<*>>()
 
-
-    fun <T : Any> arg(kClass: KClass<T>, block : InputValueDSL<T>.() -> Unit){
-        inputValues.add(InputValueDSL(kClass).apply(block))
+    fun <T : Any> arg(kClass: KClass<T>, kType: KType? = null, block : InputValueDSL<T>.() -> Unit){
+        inputValues.add(InputValueDSL(kClass, kType).apply(block))
     }
 
+    @OptIn(ExperimentalStdlibApi::class)
     inline fun <reified T : Any> arg(noinline block : InputValueDSL<T>.() -> Unit){
-        arg(T::class, block)
+        arg(T::class, typeOf<T>(), block)
     }
 
 }
