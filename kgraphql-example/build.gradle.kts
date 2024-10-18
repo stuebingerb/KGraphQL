ffb26d1005931dcbaa1732af11e8af5903a464e6
Mervyn McCreight <mervyn-mccreight@users.noreply.github.com>
2024-10-18 17:23:38 +0200

    Use version catalog

--- kgraphql-example/build.gradle.kts
+++ kgraphql-example/build.gradle.kts
@@ -7,25 +7,18 @@ kotlin {
     jvmToolchain(21)
 }
 
-val ktor_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val h2_version: String by project
-val hikari_version: String by project
-val junit_version: String by project
-
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 }
 
 dependencies {
     implementation(project(":kgraphql-ktor"))
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
-    implementation("com.h2database:h2:$h2_version")
-    implementation("com.zaxxer:HikariCP:$hikari_version")
+    implementation(libs.ktor.server.netty)
+    implementation(libs.ktor.server.auth)
+    implementation(libs.logback.classic)
+    implementation(libs.exposed.core)
+    implementation(libs.exposed.jdbc)
+    implementation(libs.exposed.javaTime)
+    implementation(libs.h2)
+    implementation(libs.hikari)
 }
--- kgraphql-ktor/build.gradle.kts
+++ kgraphql-ktor/build.gradle.kts
@@ -1,34 +1,19 @@
 plugins {
     id("library-conventions")
-    kotlin("plugin.serialization") version "2.0.21"
+    alias(libs.plugins.serialization)
 }
-
-val caffeine_version: String by project
-val kDataLoader_version: String by project
-val kotlin_version: String by project
-val serialization_version: String by project
-val coroutine_version: String by project
-val jackson_version: String by project
-val ktor_version: String by project
-
-val netty_version: String by project
-val hamcrest_version: String by project
-val kluent_version: String by project
-val junit_version: String by project
 
 val isReleaseVersion = !version.toString().endsWith("SNAPSHOT")
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
     api(project(":kgraphql"))
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("com.github.salomonbrys.kotson:kotson:2.5.0")
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.ktor.server.core)
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:$junit_version")
-    testImplementation("org.amshove.kluent:kluent:$kluent_version")
-    testImplementation("io.ktor:ktor-server-test-host:$ktor_version")
-    testImplementation("io.ktor:ktor-server-auth:$ktor_version")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junit_version")
+    testImplementation(libs.junit.jupiter.api)
+    testImplementation(libs.kluent)
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.ktor.server.auth)
+    testRuntimeOnly(libs.junit.jupiter.engine)
 }
