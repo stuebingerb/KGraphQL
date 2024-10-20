03ae5da5ae40ed72c4c5b11c4f47f4290342c05f
Mervyn McCreight <mervyn-mccreight@users.noreply.github.com>
2024-10-20 20:37:51 +0200

    build(examples): remove non-working netty example

--- kgraphql/src/test/kotlin/com/apurebase/kgraphql/demo/HelloWorld.kt
+++ kgraphql/src/test/kotlin/com/apurebase/kgraphql/demo/HelloWorld.kt
@@ -2,7 +2,7 @@ package com.apurebase.kgraphql.demo
 
 import com.apurebase.kgraphql.KGraphQL
 
-fun main() {
+suspend fun main() {
     val schema = KGraphQL.schema {
         query("hello") {
             resolver { name: String -> "Hello, $name" }
@@ -10,5 +10,5 @@ fun main() {
     }
 
     //prints '{"data":{"hello":"Hello, Ted Mosby"}}'
-    println(schema.executeBlocking("{hello(name : \"Ted Mosby\")}"))
+    println(schema.execute("{hello(name : \"Ted Mosby\")}"))
 }
