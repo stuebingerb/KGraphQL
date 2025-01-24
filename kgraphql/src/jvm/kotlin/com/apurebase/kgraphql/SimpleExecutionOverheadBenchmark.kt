7024eb45632fbc86f2ca0166c7dec9ee1d8f0d90
Mervyn McCreight <mervyn-mccreight@users.noreply.github.com>
2025-01-24 17:12:06 +0100

    test(benchmark): make benchmarks runnable
    The benchmarks have been excluded from the
    build for quite a while. Repair and re-add the
    benchmarks to the Gradle build.

--- kgraphql/src/jvm/kotlin/com/apurebase/kgraphql/SimpleExecutionOverheadBenchmark.kt
+++ kgraphql/src/jvm/kotlin/com/apurebase/kgraphql/SimpleExecutionOverheadBenchmark.kt
@@ -6,16 +6,21 @@ import com.apurebase.kgraphql.BenchmarkSchema.twoResolver
 import com.apurebase.kgraphql.schema.Schema
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.benchmark.Benchmark
+import kotlinx.benchmark.Measurement
+import kotlinx.benchmark.OutputTimeUnit
+import kotlinx.benchmark.Param
+import kotlinx.benchmark.Scope
+import kotlinx.benchmark.Setup
+import kotlinx.benchmark.State
+import kotlinx.benchmark.Warmup
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Test
-import org.openjdk.jmh.annotations.*
 import java.util.concurrent.TimeUnit
 
 @State(Scope.Benchmark)
 @Warmup(iterations = 15)
 @Measurement(iterations = 3)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Fork(value = 5)
 open class SimpleExecutionOverheadBenchmark {
 
     @Param("true", "false")
@@ -36,26 +41,14 @@ open class SimpleExecutionOverheadBenchmark {
 
     @Benchmark
     fun benchmark(): String {
-        if (withKGraphQL) {
-            return schema.executeBlocking("{one{name, quantity, active}, two(name : \"FELLA\"){range{start, endInclusive}}, three{id}}")
+        return if (withKGraphQL) {
+            schema.executeBlocking("{one{name, quantity, active}, two(name : \"FELLA\"){range{start, endInclusive}}, three{id}}")
         } else {
-            return runBlocking {
+            runBlocking {
                 ": ${objectMapper.writeValueAsString(oneResolver())} " +
-                        ": ${objectMapper.writeValueAsString(twoResolver("FELLA"))} " +
-                        ": ${objectMapper.writeValueAsString(threeResolver())}"
+                    ": ${objectMapper.writeValueAsString(twoResolver("FELLA"))} " +
+                    ": ${objectMapper.writeValueAsString(threeResolver())}"
             }
         }
-    }
-
-    @Test
-    fun testWithKGraphQL() {
-        setup()
-        println(benchmark())
-    }
-
-    @Test
-    fun testNoKGraphQL() {
-        withKGraphQL = false
-        println(benchmark())
     }
 }
