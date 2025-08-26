0d499f8bd9045000cf480445ca6ea9877cf3a1f3
Bernd Stübinger <41049452+stuebingerb@users.noreply.github.com>
2025-08-26 17:40:23 +0200

    feat!: support dataProperty with ParallelRequestExecutor
    Adds proper handling for `dataProperties` to `ParallelRequestExecutor`
    and gets rid of `DataLoaderPreparedRequestExecutor`, along with the
    option to configure the request executor in `SchemaConfiguration` and
    related `DataLoaderPreparedRequestExecutor`-specific options.
    
    This also consolidates some tests that were hardcoded to use one of
    the request executors, and therefore missed the other, and extends
    coverage for dataProperties a bit. Further, this reduces visibility of
    some functions to `internal` to reduce namespace pollution.
    
    Resolves #58
    Resolves #268
    
    BREAKING CHANGE: The option to configure request executors has been
    removed, and execution will now always use the `ParallelRequestExecutor`.
    BREAKING CHANGE: The option to configure a timeout has been removed,
    as it was only respected by `DataLoaderPreparedRequestExecutor`.
    BREAKING CHANGE: The possibility to pass `ExecutionOptions` to schema
    execution has been removed, as it was only respected by `DataLoaderPreparedRequestExecutor`.
    BREAKING CHANGE: The `DataLoaderPreparedRequestExecutor`, `Executor`,
    and `ExecutionOptions` classes have been removed.

--- kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/execution/ExecutionPlan.kt
+++ kgraphql/src/main/kotlin/com/apurebase/kgraphql/schema/execution/ExecutionPlan.kt
@@ -1,7 +1,6 @@
 package com.apurebase.kgraphql.schema.execution
 
 class ExecutionPlan(
-    val options: ExecutionOptions,
     val operations: List<Execution.Node>
 ) : List<Execution.Node> by operations {
     var isSubscription = false
