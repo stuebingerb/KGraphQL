7620cc46e2ba3f1d34a12ccd6873b988e9ca02e4
Bernd Stübinger <41049452+stuebingerb@users.noreply.github.com>
2025-09-26 14:13:20 +0200

    chore: remove some unused stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("com.github.ben-manes.versions") version "0.52.0"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.18.1"
     alias(libs.plugins.kotlinx.kover)
     jacoco
